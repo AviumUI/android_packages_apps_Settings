b73ea258cd155f7a75e49f9d3a298bd860aa7c20
ruyue <ruyueee@outlook.com>
2025-08-23 14:55:11 +0800

    feat(DeviceInfo): Add custom device info card and header layout
    Change-Id: I70f7f0f233b53532a07760ccfe36640eaf1433f8

---
+++ src/org/avium/settings/aboutphone/AviumDeviceInfoCardController.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2025-2026 The AviumUI Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package org.avium.settings.aboutphone;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StatFs;
+import android.os.SystemProperties;
+import android.os.storage.StorageManager;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.widget.LayoutPreference;
+import com.android.settingslib.deviceinfo.StorageManagerVolumeProvider;
+
+import java.io.File;
+
+public class AviumDeviceInfoCardController extends AbstractPreferenceController {
+
+    private LayoutPreference mLayoutPreference;
+    private static final String UNKNOWN = "Unknown";
+
+    public AviumDeviceInfoCardController(Context context, String key) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return "avium_device_info_card";
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mLayoutPreference = screen.findPreference(getPreferenceKey());
+        if (mLayoutPreference != null) {
+            updateState(mLayoutPreference);
+        }
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        if (mLayoutPreference == null) {
+            return;
+        }
+
+        // model
+        TextView modelView = mLayoutPreference.findViewById(R.id.device_model_value);
+        modelView.setText(SystemProperties.get("ro.product.model", Build.MODEL));
+
+        // soc
+        TextView processorView = mLayoutPreference.findViewById(R.id.processor_value);
+        processorView.setText(SystemProperties.get("ro.soc.model", UNKNOWN));
+
+        // android version
+        TextView androidVersionView = mLayoutPreference.findViewById(R.id.android_version_value);
+        androidVersionView.setText(Build.VERSION.RELEASE);
+
+        // ram
+        TextView ramView = mLayoutPreference.findViewById(R.id.ram_value);
+        ramView.setText(getTotalRam());
+
+        // rom
+        TextView storageView = mLayoutPreference.findViewById(R.id.storage_value);
+        storageView.setText(getStorageInfo());
+
+        // ssystem version
+        TextView systemVersionView = mLayoutPreference.findViewById(R.id.system_version_value);
+        systemVersionView.setText(getSystemDisplayVersion());
+
+        // screen
+        TextView resolutionView = mLayoutPreference.findViewById(R.id.screen_resolution_value);
+        resolutionView.setText(getScreenResolution());
+    }
+
+    private String getTotalRam() {
+        ActivityManager actManager = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+        actManager.getMemoryInfo(memInfo);
+        double totalRamGbDecimal = memInfo.totalMem / 1_000_000_000d;
+
+        final int[] buckets = new int[] { 2, 4, 6, 8, 12, 16, 24 };
+        int best = buckets[0];
+        double bestDiff = Double.MAX_VALUE;
+        for (int b : buckets) {
+            double diff = Math.abs(totalRamGbDecimal - b);
+            if (diff < bestDiff) {
+                bestDiff = diff;
+                best = b;
+            }
+        }
+        return best + " GB";
+    }
+
+    private String getStorageInfo() {
+        try {
+            long totalBytes = 0L;
+            StorageManager sm = mContext.getSystemService(StorageManager.class);
+            if (sm != null) {
+                StorageManagerVolumeProvider smvp = new StorageManagerVolumeProvider(sm);
+                totalBytes = smvp.getPrimaryStorageSize();
+            }
+
+            if (totalBytes <= 0) {
+                File totalPath = Environment.getDataDirectory();
+                StatFs totalStat = new StatFs(totalPath.getPath());
+                totalBytes = totalStat.getBlockSizeLong() * totalStat.getBlockCountLong();
+            }
+
+            File path = Environment.getDataDirectory();
+            StatFs stat = new StatFs(path.getPath());
+            long freeBytes = stat.getAvailableBytes();
+
+            long totalG = Math.round(totalBytes / 1_000_000_000d);
+            long freeG = (long) Math.floor(freeBytes / 1_000_000_000d);
+            if (freeG < 0) freeG = 0;
+            long usedG = totalG - freeG;
+
+            return usedG + "/" + totalG + "G";
+        } catch (Exception e) {
+            return UNKNOWN;
+        }
+    }
+
+    private String getSystemDisplayVersion() {
+        return SystemProperties.get("ro.avium.display.version", UNKNOWN);
+    }
+
+    private String getScreenResolution() {
+        try {
+            WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+            final DisplayMetrics metrics = new DisplayMetrics();
+            if (wm != null) {
+                wm.getDefaultDisplay().getRealMetrics(metrics);
+                return metrics.heightPixels + " x " + metrics.widthPixels;
+            }
+        } catch (Exception e) {
+        }
+        return UNKNOWN;
+    }
+}
---
+++ src/org/avium/settings/aboutphone/AviumHeaderController.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2025-2026 The AviumUI Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package org.avium.settings.aboutphone;
+
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.widget.LayoutPreference;
+
+public class AviumHeaderController extends AbstractPreferenceController {
+
+    private static final String TAG = "AviumHeaderController";
+    private static final float BLUR_RADIUS = 30f;
+    private LayoutPreference mLayoutPreference;
+
+    public AviumHeaderController(Context context, String key) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return "avium_header";
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mLayoutPreference = screen.findPreference(getPreferenceKey());
+        if (mLayoutPreference != null) {
+            updateState(mLayoutPreference);
+        }
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        if (mLayoutPreference == null) {
+            return;
+        }
+        ImageView wallpaperImageView = mLayoutPreference.findViewById(R.id.wallpaper_blur_image);
+        if (wallpaperImageView != null) {
+            try {
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+                Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+
+                if (wallpaperDrawable == null) {
+                    //Log.e(TAG, "Wallpaper null!");
+                    return;
+                }
+
+                Bitmap wallpaperBitmap = drawableToBitmap(wallpaperDrawable);
+                Bitmap blurredBitmap = blurBitmap(mContext, wallpaperBitmap, BLUR_RADIUS);
+                wallpaperImageView.setImageBitmap(blurredBitmap);
+                //Log.d(TAG, "Wallpaper successfully");
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to blur wallpaper", e);
+            }
+        }
+    }
+
+    private static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        int width = drawable.getIntrinsicWidth();
+        width = width > 0 ? width : 1;
+        int height = drawable.getIntrinsicHeight();
+        height = height > 0 ? height : 1;
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    //blur
+    private static Bitmap blurBitmap(Context context, Bitmap bitmap, float radius) {
+        Bitmap outputBitmap = Bitmap.createBitmap(bitmap);
+        RenderScript rs = RenderScript.create(context);
+        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        Allocation allIn = Allocation.createFromBitmap(rs, bitmap);
+        Allocation allOut = Allocation.createFromBitmap(rs, outputBitmap);
+        blurScript.setRadius(radius > 25f ? 25f : radius);  //blur raduis
+        blurScript.setInput(allIn);
+        blurScript.forEach(allOut);
+        allOut.copyTo(outputBitmap);
+        rs.destroy();
+        return outputBitmap;
+    }
+}
